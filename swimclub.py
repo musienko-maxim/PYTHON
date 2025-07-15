26f27e59dc0dbac899f82fd35aec8a244332815b
mmusiien <mmusiien@redhat.com>
2025-07-15 16:09:24 +0300

    add content

---
+++ swimclub.py
@@ -0,0 +1,24 @@
+import statistics
+FOLDER = "swimdata/"
+def read_swim_data (filename):
+ swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+ with open(FOLDER + filename) as file:
+  lines = file.readlines()
+  times = lines[0].strip().split(",")
+ converts = []
+ for t in times:
+  if ":" in t:
+    minutes, rest = t.split(":")
+    seconds, hundredts = rest.split(".")
+  else:
+    minutes = 0
+    seconds, hundredts = t.split(".")
+ converts.append((int(minutes)*60*100 + (int(seconds)*100) + int(hundredts)))
+ 
+ average = statistics.mean(converts)
+ min_secs, hundredths=str(round(average/100,2)).split(".")
+ min_secs = int(min_secs)
+ minutes = min_secs//60
+ seconds = min_secs - minutes*60
+ average=str(minutes) + ":" + str(seconds) + "."+ hundredths
+ return swimmer, age, distance, stroke, times, average
